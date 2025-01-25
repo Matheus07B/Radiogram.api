dc944604104cf7de97d6ace41494c9ff120b3c4d
Matheus07B <matheusveneski654@gmail.com>
2025-01-25 03:27:41 -0300

    01 - Test!

--- database/schema.sql
+++ database/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 CREATE TABLE IF NOT EXISTS friendships (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    user_name TEXT NOT NULL
+    user_name TEXT NOT NULL,
     friend_id INTEGER NOT NULL,
     friend_name TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES usuarios (id),
