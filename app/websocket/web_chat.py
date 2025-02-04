8f5126224d51267a73db8d3dacb8a04159fee6dc
Matheus07B <matheusveneski654@gmail.com>
2025-02-04 07:31:48 -0300

    4 - Websocket add in API!

--- app/websocket/web_chat.py
+++ app/websocket/web_chat.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, request, jsonify
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
