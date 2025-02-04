b11332d3a79f44f905a86e4a98fc28eecfc8b64c
Matheus07B <matheusveneski654@gmail.com>
2025-02-04 08:47:38 -0300

    4.4 - Websocket add in API!

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from app.models.database import init_db
 from app.routes import register_routes
+from app.websocket.socket_server import configure_websocket  # Importa o WebSocket
 
 def create_app():
     app = Flask(__name__)
@@ -17,4 +18,30 @@ def create_app():
     # Registra as rotas
     register_routes(app)
 
-    return app
+    # Configura o WebSocket dentro da API
+    socketio = configure_websocket(app)
+
+    return app, socketio  # Retorna o WebSocket junto com a app
+
+##############################################################################
+
+# from flask import Flask
+# from flask_cors import CORS
+# from app.models.database import init_db
+# from app.routes import register_routes
+
+# def create_app():
+#     app = Flask(__name__)
+#     CORS(app)
+
+#     # Carrega as configurações
+#     app.config.from_object('config.Config')  # Certifique-se de referenciar a classe Config corretamente
+
+#     # Inicializa o banco de dados dentro do contexto da app
+#     with app.app_context():
+#         init_db()
+
+#     # Registra as rotas
+#     register_routes(app)
+
+#     return app
--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from app import create_app
 
-app = create_app()
+app, socketio = create_app()  # Obtém a app Flask e o WebSocket
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="0.0.0.0", port=5001)
+
+##########################################
+
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
