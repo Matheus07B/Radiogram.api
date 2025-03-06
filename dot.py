73f0090defe28ca27a4b8de724cad29cbfe9dbcc
Matheus07B <matheusveneski654@gmail.com>
2025-03-06 03:54:42 -0300

    .env in Render!

--- dot.py
+++ dot.py
@@ -1,4 +1,4 @@
-from dotenv import dotenv_values
+# from dotenv import dotenv_values
 
-config = dotenv_values(".env")  # Carrega as variáveis manualmente
-print(config)  # Veja se SECRET_KEY aparece corretamente
+# config = dotenv_values(".env")  # Carrega as variáveis manualmente
+# print(config)  # Veja se SECRET_KEY aparece corretamente
